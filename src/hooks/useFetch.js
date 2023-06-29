5d5871cc6af2a2bd62a0115a6dce3a096c7e37df
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-06-28 17:59:34 -0700

    useFetch data initialized set to {}

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { makeRequest } from "../makeRequest";
 
 const useFetch = (url) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
