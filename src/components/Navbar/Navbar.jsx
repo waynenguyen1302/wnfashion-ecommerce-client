5d340018e1aa479a7f654af72eafeb5a637cf66f
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-06-29 00:15:39 -0700

    fixed missing image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="wrapper">
         <div className="left">
           <div className="item">
-            <img src="/img/en.png" alt="" />
+            <img src="/img/eng.png" alt="" />
             <KeyboardArrowDownIcon />
           </div>
           <div className="item">
